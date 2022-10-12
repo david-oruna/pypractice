9470abc4d2826b45469405657368d4685e6f6a37
David Hawk <104524650+david-oruna@users.noreply.github.com>
2022-10-11 22:25:36 -0500

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,13 +1,15 @@
-def getdata():
+def getstudent():
+    # Dict variable "D"
     D = {}
     print(D)
     while True:
         student_id = input("Enter id: ")
         marks_list = input('Enters the marks sepparated by commas: ')
         moreStudents = input('Do you want to save results: ')
+        # Let's check if student is in list
         if student_id in D:
-            print('Study already enrolled')
+            print('Already')
         else:
             D[student_id] = marks_list.split(",")
-        if moreStudents.lower() == 'no':
+        if moreStudens.lower == "no":
             return
