32859befccb0fdbe959df3e4b37a8fba97c270bc
David Hawk <104524650+david-oruna@users.noreply.github.com>
2022-10-20 21:25:45 -0500

    Create khangame.py

---
+++ Projects.txt/khangame.py
@@ -0,0 +1,34 @@
+# Guessing number game
+
+import random 
+
+
+
+def khang():
+    while True:
+        try:
+        
+            x = range(1,100)
+            n = random.randint(1, 100)
+            lol = int(input('Enter a number from 1-100: '))
+            if lol in x :
+                print('Number')
+                if lol > n:
+                    print('Too high')
+                    continue
+                if lol < n:
+                    print('Too low')
+                    continue
+                else:
+                    ('you got it 😎')
+                    break
+
+                   
+            else:
+                print('try again')
+                continue
+
+        except ValueError as e:
+            print(f'{e} Try again')
+ 
+khang()
